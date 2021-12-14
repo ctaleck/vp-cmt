01550bbdab141fff1c1dbea5a4399273dcaf968a
Christopher Taleck <chris@taleck.com>
2021-12-14 08:34:54 -0700

    lab 13 bonus 18 step 2-17

--- tools/generators/feature-lib/index.ts
+++ tools/generators/feature-lib/index.ts
@@ -1,8 +1,17 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+export interface Schema {
+  name: string;
+  directory: 'store' | 'api' | 'shared';
+} 
+
+export default async function (tree: Tree, schema: Schema) {
+  await libraryGenerator(tree, { 
+    name: `feature-${schema.name}`,
+    directory: `${schema.directory}`,
+    tags: `type:feature, scope:${schema.directory}` 
+  });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
