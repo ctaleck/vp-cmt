4f790f58f880c5a6a6d3100a17716efbdd2ddb46
Christopher Taleck <chris@taleck.com>
2021-12-13 19:30:37 -0700

    add util-lib generator

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -2,7 +2,13 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  const name = `util-${schema.name}`;
+  const directory = `${schema.directory}`;
+  const type = `type:util`;
+  const scope = `scope:${directory}`;
+  const tags = `${type}, ${scope}`;
+  console.log(`You are creating a utility library called '${name}' with tags '${tags}' in '${directory}'`);
+  await libraryGenerator(tree, { name, directory, tags});
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
